5f75535bd46c84844369b3412b34039f36c09798
mouchette <romain.desjardins@gmail.com>
2021-08-28 14:09:21 +0200

    cc rom

--- Assets/Scripts/GetBarril.cs
+++ Assets/Scripts/GetBarril.cs
@@ -5,9 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class GetBarril : MonoBehaviour
 {
-    bool stopCollide = false;
-
-    public int countBarril = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +14,5 @@ public class GetBarril : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (countBarril > 2)
-            SceneManager.LoadScene("Victoire");
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.gameObject.tag == "barril" && !stopCollide) {
-            stopCollide = true;
-            countBarril++;
-
-            GameObject.Find("Main Camera").GetComponent<SpawnBarril>().moving = false;
-
-            Destroy(collision.gameObject);
-
-            stopCollide = false;
-        }
-            
     }
 }
