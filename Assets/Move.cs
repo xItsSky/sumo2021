6c6055de49a2aad1b2b9a38df3ad47ac4d35fb1c
quentin <emo.quentin@gmail.com>
2021-08-28 04:49:16 +0200

    FEAT: Add some animation to blue sumo

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
     public float speed = 5f;
+    public bool facingRight;
     public bool isGrounded = false;
+    public Animator animator;
 
     void Start(){
     }
@@ -14,6 +16,12 @@ public class Move : MonoBehaviour
     {
 
         Jump();
+        this.flip(Input.GetAxis("Horizontal"));
+
+        if (Input.GetAxis("Horizontal") != 0)
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
 
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
@@ -25,6 +33,20 @@ public class Move : MonoBehaviour
         if(Input.GetKeyDown("space") && isGrounded == true)
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+    }
+
+    void flip(float move)
+    {
+        if (move < 0)
+        {
+            this.facingRight = false;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if(move > 0)
+        {
+            this.facingRight = true;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
     }
 }
